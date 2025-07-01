65a2202abc58b257d7359f2278bc42a9ca1f8484
Ali Akbari <alieakbariy@gmil.com>
2025-07-01 20:38:35 +0330

    add script for install arch linux

---
+++ scripts/run_once_install_packages.sh
@@ -0,0 +1,6 @@
+
+# install WM packages
+sudo pacman -S sway swaybg foot polkit wofi waybar
+
+# install esential packages
+sudo pacman -S firefox neovim eza zoxide bat htop btop playerctl yazi wildmidi lollypop
